48494b65bbb1c88749ee1ed20ca763e3abb5b6df
unknown <aitraho.anas@gmail.com>
2023-02-24 12:38:54 +0100

    third commit

--- src/main/java/net/izicap/anas/aitraho/chatgptproject/controller/QuestionAnswerController.java
+++ src/main/java/net/izicap/anas/aitraho/chatgptproject/controller/QuestionAnswerController.java
@@ -14,21 +14,28 @@ import net.izicap.anas.aitraho.chatgptproject.service.QuestionAnswerService;
 import java.io.IOException;
 
 
+//Declare the class as a REST controller and add the @Slf4j annotation for logging
 @Slf4j
 @RestController
-public class QuestionAnswerController {
+public class QuestionAnswerController 
+{
+	// Declare a private instance variable for the QuestionAnswerService
 	 private QuestionAnswerService questionAnswerService;
     
+	// Constructor that takes a QuestionAnswerService object as a parameter and assigns it to the instance variable
     public QuestionAnswerController(QuestionAnswerService questionAnswerService) {
 		this.questionAnswerService = questionAnswerService;
 	}
-
+    
+    // Define a POST endpoint with the mapping "/getAnswer" that takes a ChatRequest object in the request body
 	@PostMapping("/getAnswer")
     public ChatResponse getAnswer( @RequestBody ChatRequest chatRequest) {
 
         try {
+        	// Call the getAnswer method on the QuestionAnswerService object, passing in the ChatRequest object
 			return questionAnswerService.getAnswer(chatRequest);
 		} catch (IOException e) {
+			// Log an error message if an IOException is thrown and return a ChatResponse object with an error message
 			e.printStackTrace();
 			log.error("Error getting answer from chatGPT");
 			return new ChatResponse("Error getting answer from chatGPT");
@@ -36,4 +43,3 @@ public class QuestionAnswerController {
     }
 
 }
-
